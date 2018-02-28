ecf4314e2a9509e748d9bd0d540f1eac418117bc
kriri <kristina11@abv.bg>
2018-02-28 19:36:58 +0200

    Initial commit

---
+++ src/main/java/hello/Main.java
@@ -0,0 +1,9 @@
+package main.java.hello;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Singleton s = Singleton.Instance();
+	}
+
+}
---
+++ src/main/java/hello/Singleton.java
@@ -0,0 +1,15 @@
+package main.java.hello;
+
+public class Singleton {
+	private static Singleton theInstance = null;
+
+	private Singleton() {
+	}
+
+	public static Singleton Instance() {
+
+		if (theInstance == null)
+			theInstance = new Singleton();
+		return theInstance;
+	}
+}
